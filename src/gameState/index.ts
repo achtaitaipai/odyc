07a8424d944db6152f0321afb90dee32a908d0f1
achtaitaipai <charles.cailleteau@gmail.com>
2024-03-29 11:28:13 +0100

    remove useless code

--- src/gameState/index.ts
+++ src/gameState/index.ts
@@ -1,6 +1,4 @@
-import { createGridFromString, getGridSize } from '../lib/index.js'
 import { createActorsStore } from './actors.js'
-import { createCounts } from './counts.js'
 import { createMapStore } from './map.js'
 import { createPlayer } from './player.js'
 import { GameStateParams, Templates } from './types.js'
@@ -10,7 +8,6 @@ export type GameState<T extends Templates> = ReturnType<typeof initGameState<T>>
 export const initGameState = <U extends Templates>(
 	params: GameStateParams<U>,
 ) => {
-	const mapDimensions = getGridSize(createGridFromString(params.map))
 	const mapStore = createMapStore(params.map)
 	const player = createPlayer(params.player)
 	const actors = createActorsStore<U>(params, mapStore)
