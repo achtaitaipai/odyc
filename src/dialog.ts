ef1d84776b8f64a144765683f8f1152e58870bc0
achtaitaipai <charles.cailleteau@gmail.com>
2024-04-02 15:44:04 +0200

    private methods in dialog and messageBox

--- src/dialog.ts
+++ src/dialog.ts
@@ -1,100 +1,101 @@
 export class Dialog {
-	wrapperElement: HTMLElement
-	boxElement: HTMLElement
-	lines: [HTMLElement, HTMLElement]
-	resolvePromise?: () => void
-	text?: string[]
-	lineCursor = 0
-	currentChunk?: string[]
+	private _wrapperElement: HTMLElement
+	private boxElement: HTMLElement
+	private _lines: [HTMLElement, HTMLElement]
+	private _resolvePromise?: () => void
+	private _text?: string[]
+	private _lineCursor = 0
+	private _currentChunk?: string[]
+	private _requestAnimationFrameId?: number
+	private _lastTime = 0
+	private _timeBetweenChar = 50
+	private _charsByLine = 24
+
 	isOpen = false
 
-	requestAnimationFrameId?: number
-	lastTime = 0
-	timeBetweenChar = 50
-	charsByLine = 24
-
 	constructor() {
-		this.wrapperElement = document.createElement('div')
+		this._wrapperElement = document.createElement('div')
 		this.boxElement = document.createElement('div')
-		this.lines = [document.createElement('p'), document.createElement('p')]
+		this._lines = [document.createElement('p'), document.createElement('p')]
 		this.boxElement.style.setProperty('display', 'none')
 		this.boxElement.classList.add('odyc_dialog')
-		this.boxElement.append(...this.lines)
-		this.wrapperElement.append(this.boxElement)
+		this.boxElement.append(...this._lines)
+		this._wrapperElement.append(this.boxElement)
 
 		this._resize()
-		document.body.append(this.wrapperElement)
+		document.body.append(this._wrapperElement)
 		window.addEventListener('resize', this._resize)
-	}
-	private _resize = () => {
-		const sideSize = Math.min(window.innerWidth, window.innerHeight) * 0.9
-		const left = (window.innerWidth - sideSize) * 0.5
-		const top = (window.innerHeight - sideSize) * 0.5
-		this.wrapperElement.style.setProperty('position', 'absolute')
-		this.wrapperElement.style.setProperty('box-sizing', 'border-box')
-		this.wrapperElement.style.setProperty('width', `${sideSize}px`)
-		this.wrapperElement.style.setProperty('height', `${sideSize}px`)
-		this.wrapperElement.style.setProperty('left', `${left}px`)
-		this.wrapperElement.style.setProperty('top', `${top}px`)
 	}
 
 	async open(text: string) {
-		this.text = this.chunkText(text, this.charsByLine)
+		this._text = this._chunkText(text, this._charsByLine)
 		this.isOpen = true
 		this.boxElement.style.removeProperty('display')
 
-		this.currentChunk = this.text.shift()?.split('')
+		this._currentChunk = this._text.shift()?.split('')
 
-		this.requestAnimationFrameId = requestAnimationFrame(this.update)
+		this._requestAnimationFrameId = requestAnimationFrame(this._update)
 		return new Promise<void>((res) => {
-			this.resolvePromise = () => res()
+			this._resolvePromise = () => res()
 		})
-	}
-
-	update = (now: number) => {
-		requestAnimationFrame(this.update)
-		if (now - this.lastTime < this.timeBetweenChar) return
-		this.lastTime = now
-		if (
-			this.currentChunk?.length === 0 &&
-			this.lineCursor < this.lines.length - 1
-		) {
-			this.lineCursor++
-			this.currentChunk = this.text?.shift()?.split('')
-		}
-
-		const currentLine = this.lines[this.lineCursor]
-		const newChar = this.currentChunk?.shift()
-		if (currentLine && newChar) currentLine.textContent += newChar
 	}
 
 	next() {
 		if (!this.isOpen) return
-		if (this.currentChunk?.length === 0 || this.currentChunk === undefined) {
-			this.currentChunk = this.text?.shift()?.split('')
-			if (this.currentChunk === undefined) {
-				this.close()
+		if (this._currentChunk?.length === 0 || this._currentChunk === undefined) {
+			this._currentChunk = this._text?.shift()?.split('')
+			if (this._currentChunk === undefined) {
+				this._close()
 				return
 			}
-			this.lineCursor++
-			if (this.lineCursor >= this.lines.length) {
-				this.lineCursor = 0
-				this.lines.forEach((l) => (l.textContent = ''))
+			this._lineCursor++
+			if (this._lineCursor >= this._lines.length) {
+				this._lineCursor = 0
+				this._lines.forEach((l) => (l.textContent = ''))
 			}
 		}
 	}
 
-	close() {
-		this.isOpen = false
-		this.boxElement.style.setProperty('display', 'none')
-		this.resolvePromise?.()
-		this.lines.forEach((l) => (l.textContent = ''))
-		this.lineCursor = 0
-		this.requestAnimationFrameId &&
-			cancelAnimationFrame(this.requestAnimationFrameId)
+	private _update = (now: number) => {
+		requestAnimationFrame(this._update)
+		if (now - this._lastTime < this._timeBetweenChar) return
+		this._lastTime = now
+		if (
+			this._currentChunk?.length === 0 &&
+			this._lineCursor < this._lines.length - 1
+		) {
+			this._lineCursor++
+			this._currentChunk = this._text?.shift()?.split('')
+		}
+
+		const currentLine = this._lines[this._lineCursor]
+		const newChar = this._currentChunk?.shift()
+		if (currentLine && newChar) currentLine.textContent += newChar
 	}
 
-	chunkText(text: string, chunckLength: number) {
+	private _resize = () => {
+		const sideSize = Math.min(window.innerWidth, window.innerHeight) * 0.9
+		const left = (window.innerWidth - sideSize) * 0.5
+		const top = (window.innerHeight - sideSize) * 0.5
+		this._wrapperElement.style.setProperty('position', 'absolute')
+		this._wrapperElement.style.setProperty('box-sizing', 'border-box')
+		this._wrapperElement.style.setProperty('width', `${sideSize}px`)
+		this._wrapperElement.style.setProperty('height', `${sideSize}px`)
+		this._wrapperElement.style.setProperty('left', `${left}px`)
+		this._wrapperElement.style.setProperty('top', `${top}px`)
+	}
+
+	private _close() {
+		this.isOpen = false
+		this.boxElement.style.setProperty('display', 'none')
+		this._resolvePromise?.()
+		this._lines.forEach((l) => (l.textContent = ''))
+		this._lineCursor = 0
+		this._requestAnimationFrameId &&
+			cancelAnimationFrame(this._requestAnimationFrameId)
+	}
+
+	private _chunkText(text: string, chunckLength: number) {
 		const regex = new RegExp(
 			`(.{1,${chunckLength}})( +\|$\\n?)\|(.{1,${chunckLength}})`,
 			'gm',
--- src/messageBox.ts
+++ src/messageBox.ts
@@ -1,46 +1,46 @@
 export class MessageBox {
-	wrapperElement: HTMLElement
+	private _wrapperElement: HTMLElement
 	isOpen = false
-	resolePromise?: () => void
+	private _resolePromise?: () => void
 
 	constructor() {
-		this.wrapperElement = document.createElement('div')
-		this.wrapperElement.classList.add('odyc_messageBox')
-		this.wrapperElement.style.setProperty('display', 'none')
+		this._wrapperElement = document.createElement('div')
+		this._wrapperElement.classList.add('odyc_messageBox')
+		this._wrapperElement.style.setProperty('display', 'none')
 
 		this._resize()
 		window.addEventListener('resize', this._resize)
 
-		document.body.append(this.wrapperElement)
+		document.body.append(this._wrapperElement)
 	}
 
 	private _resize = () => {
 		const sideSize = Math.min(window.innerWidth, window.innerHeight) * 0.9
 		const left = (window.innerWidth - sideSize) * 0.5
 		const top = (window.innerHeight - sideSize) * 0.5
-		this.wrapperElement.style.setProperty('position', 'absolute')
-		this.wrapperElement.style.setProperty('box-sizing', 'border-box')
-		this.wrapperElement.style.setProperty('width', `${sideSize}px`)
-		this.wrapperElement.style.setProperty('height', `${sideSize}px`)
-		this.wrapperElement.style.setProperty('left', `${left}px`)
-		this.wrapperElement.style.setProperty('top', `${top}px`)
+		this._wrapperElement.style.setProperty('position', 'absolute')
+		this._wrapperElement.style.setProperty('box-sizing', 'border-box')
+		this._wrapperElement.style.setProperty('width', `${sideSize}px`)
+		this._wrapperElement.style.setProperty('height', `${sideSize}px`)
+		this._wrapperElement.style.setProperty('left', `${left}px`)
+		this._wrapperElement.style.setProperty('top', `${top}px`)
 	}
 
 	open(text: string) {
 		this.isOpen = true
 		if (!text) return
-		this.wrapperElement.style.removeProperty('display')
+		this._wrapperElement.style.removeProperty('display')
 		const p = document.createElement('p')
 		p.textContent = text
-		this.wrapperElement.append(p)
-		return new Promise<void>((res) => (this.resolePromise = () => res()))
+		this._wrapperElement.append(p)
+		return new Promise<void>((res) => (this._resolePromise = () => res()))
 	}
 
 	close = () => {
 		this.isOpen = false
-		this.wrapperElement.style.setProperty('display', 'none')
-		this.wrapperElement.innerHTML = ''
-		this.resolePromise?.()
+		this._wrapperElement.style.setProperty('display', 'none')
+		this._wrapperElement.innerHTML = ''
+		this._resolePromise?.()
 	}
 }
 
