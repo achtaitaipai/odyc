b824aa2506eb9058bf34f82298203abd7fb64673
Matej Bačo <matejbaco2000@gmail.com>
2025-06-18 20:42:09 +0200

    Attempt to cause CI/CD issue

--- tests/visual/player-size/index.ts
+++ tests/visual/player-size/index.ts
@@ -5,8 +5,6 @@ import { createGame } from '../../../dist/index'
 export const init = () => {
 	const game = createGame({
 		map: ``,
-		screenWidth: 2,
-		screenHeight: 2,
 	})
 
 	return { game, state }
