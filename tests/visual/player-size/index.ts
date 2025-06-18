fc513324c2c98299e69df29391c9f394ea5a883d
Matej Bačo <matejbaco2000@gmail.com>
2025-06-18 20:47:00 +0200

    Hardcode failure settings

--- tests/visual/player-size/index.ts
+++ tests/visual/player-size/index.ts
@@ -5,6 +5,8 @@ import { createGame } from '../../../dist/index'
 export const init = () => {
 	const game = createGame({
 		map: ``,
+		screenWidth: 8,
+		screenHeight: 8,
 	})
 
 	return { game, state }
