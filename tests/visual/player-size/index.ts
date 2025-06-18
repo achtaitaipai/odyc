cc5fa3e17a1e769d4a003140c68424466fe752d5
Matej Bačo <matejbaco2000@gmail.com>
2025-06-18 20:51:57 +0200

    Re-cause the problem

--- tests/visual/player-size/index.ts
+++ tests/visual/player-size/index.ts
@@ -5,8 +5,8 @@ import { createGame } from '../../../dist/index'
 export const init = () => {
 	const game = createGame({
 		map: ``,
-		screenWidth: 7,
-		screenHeight: 7,
+		screenWidth: 8,
+		screenHeight: 8,
 	})
 
 	return { game, state }
