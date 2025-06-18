798d334a7865eb24b030bc9736dbfd31f4cd272f
Matej Bačo <matejbaco2000@gmail.com>
2025-06-18 20:50:52 +0200

    attempt CI/CD visual bug fix

--- tests/visual/player-size/index.ts
+++ tests/visual/player-size/index.ts
@@ -5,8 +5,8 @@ import { createGame } from '../../../dist/index'
 export const init = () => {
 	const game = createGame({
 		map: ``,
-		screenWidth: 6,
-		screenHeight: 6,
+		screenWidth: 7,
+		screenHeight: 7,
 	})
 
 	return { game, state }
