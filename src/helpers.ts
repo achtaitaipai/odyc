ba28f119b13874f2b70199fbbcc01e3aa68461a0
achtaitaipai <charles.cailleteau@gmail.com>
2025-06-19 21:34:49 +0200

    feat: charToSprite helper

---
+++ src/helpers.ts
@@ -0,0 +1,23 @@
+import { characters, FONT_SIZE } from './lib'
+
+export function charToSprite(char: string, color?: string) {
+	const charCode = char.charCodeAt(0)
+	const charSet = characters.find(
+		(el) => charCode >= el.start && charCode < el.start + el.characters.length,
+	)
+	if (!charSet) return ''
+	const charTemplate = charSet.characters[charCode - charSet.start]
+	if (!charTemplate) return ''
+	let sprite = ''
+	for (let cy = 0; cy < FONT_SIZE; cy++) {
+		const row = charTemplate[cy]
+		if (row === undefined) continue
+		for (let cx = 0; cx < FONT_SIZE; cx++) {
+			if (row & (1 << cx)) {
+				sprite += color
+			} else sprite += '.'
+		}
+		sprite += '\n'
+	}
+	return sprite
+}
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { createGame } from './createGame.js'
 import { Template } from './gameState/types.js'
 import { createSound } from './sound.js'
 import { type ActorFacade } from './gameState/actorFacade.js'
+import { charToSprite } from './helpers.js'
 
-export { createSound }
-export { createGame }
+export { createSound, createGame, charToSprite }
+
 export type { ActorFacade as EventTarget, Template }
