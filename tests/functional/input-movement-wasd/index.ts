bdd6227f546b223a527d9afceedfee8dc48eb669
Matej Bačo <matejbaco2000@gmail.com>
2025-06-19 19:08:05 +0200

    Add WASD movement test

---
+++ tests/functional/input-movement-wasd/index.ts
@@ -0,0 +1,21 @@
+import { createGame } from '../../../dist/index'
+
+const state = {}
+
+export const init = () => {
+	const game = createGame({
+		map: `
+    ...
+    ...
+    ...
+    `,
+		screenWidth: 3,
+		screenHeight: 3,
+		player: {
+			sprite: 0,
+			position: [1, 1],
+		},
+	})
+
+	return { game, state }
+}
