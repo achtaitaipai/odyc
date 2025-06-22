9f65bb52d446b6865e5970129d9840652da3da72
achtaitaipai <charles.cailleteau@gmail.com>
2025-06-22 12:18:39 +0200

    flaky test retries

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
+		retry: 5, // Temporary solution to flaky visual tests until they can await canvas render
 		browser: {
 			viewport: { height: 512, width: 512 },
 			enabled: true,
