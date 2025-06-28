c31d1838cd4cf64201509f83ca6998cc82e39f02
achtaitaipai <charles.cailleteau@gmail.com>
2025-06-28 14:24:16 +0200

    feat: add tailwind to odyc.dev

--- apps/odyc.dev/astro.config.mjs
+++ apps/odyc.dev/astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+})
