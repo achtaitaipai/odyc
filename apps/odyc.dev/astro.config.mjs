d1625b06b3d1b35254eaaea166317ec4e8d58344
achtaitaipai <charles.cailleteau@gmail.com>
2025-06-28 14:35:20 +0200

    feat(odyc.dev): add svelte

--- apps/odyc.dev/astro.config.mjs
+++ apps/odyc.dev/astro.config.mjs
@@ -2,9 +2,13 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
+import svelte from '@astrojs/svelte'
+
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
+	integrations: [svelte()],
 })
---
+++ apps/odyc.dev/src/components/Counter.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	let count = $state(0)
+</script>
+
+<button class="border p-2" onclick={() => count++}>{count} </button>
