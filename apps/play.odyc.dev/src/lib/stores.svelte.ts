159e23edcf4872385d871cb2531a2268712430fd
achtaitaipai <charles.cailleteau@gmail.com>
2025-07-16 16:08:34 +0200

    Add reactive translation function to stores

--- apps/play.odyc.dev/src/lib/stores.svelte.ts
+++ apps/play.odyc.dev/src/lib/stores.svelte.ts
@@ -1,17 +1,26 @@
 import type { Profiles } from './appwrite';
 import { Backend, type BackendUser } from './backend';
+import { useTranslations, defaultLocale } from './i18n';
 
 let user = $state<BackendUser | undefined>(undefined);
 let profile = $state<Profiles | undefined>(undefined);
+
+const t = $derived(useTranslations(user?.prefs?.selectedLocale ?? defaultLocale));
 
 export const stores = {
 	get user() {
 		return user;
 	},
+
 	fetchUser: async () => (user = await Backend.getUserSafe()),
 
 	get profile() {
 		return profile;
 	},
-	fetchProfile: async (profileId: string) => (profile = await Backend.getProfile(profileId))
+
+	fetchProfile: async (profileId: string) => (profile = await Backend.getProfile(profileId)),
+
+	get t() {
+		return t;
+	}
 };
