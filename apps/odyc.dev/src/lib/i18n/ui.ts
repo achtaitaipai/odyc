84c98e8d27d38676c66989bdb01b25a8d1381804
achtaitaipai <charles.cailleteau@gmail.com>
2025-07-18 15:47:27 +0200

    feat: search component

--- apps/odyc.dev/src/lib/i18n/ui.ts
+++ apps/odyc.dev/src/lib/i18n/ui.ts
@@ -4,9 +4,11 @@ export const translations: Record<Locale, { [key: string]: string }> = {
 	en: {
 		'nav.docs': 'Docs',
 		'nav.playground': 'Playground',
+		'ui.search': 'Search',
 	},
 	fr: {
 		'nav.docs': 'Documentation',
 		'nav.playground': 'Éditeur',
+		'ui.search': 'Rechercher',
 	},
 } as const
