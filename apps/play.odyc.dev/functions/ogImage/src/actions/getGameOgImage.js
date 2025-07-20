2ea13292165fee227f9dab6b76a6da14978b519a
Matej Bačo <matejbaco2000@gmail.com>
2025-07-20 12:58:34 +0200

    Better sowacse of OG image

--- apps/play.odyc.dev/functions/ogImage/src/actions/getGameOgImage.js
+++ apps/play.odyc.dev/functions/ogImage/src/actions/getGameOgImage.js
@@ -11,24 +11,42 @@ export async function getGameOgImage({ req, res }) {
     (
       <div
         style={{
-          fontSize: 40,
-          color: 'white',
-          background: 'black',
+          fontSize: 52,
+          color: '#EDEDF0',
+          background: '#19191D',
           width: '100%',
           height: '100%',
           padding: '50px 200px',
           textAlign: 'center',
+          fontFamily: 'Inter',
+          flexDirection: 'column',
+          gap: '-70px',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
         }}
       >
-        <h1>Hello There 👋</h1>
+        <h1 style={{ opacity: 0.6, fontSize: 40 }}>Dynamic OG images with</h1>
+        <h1 style={{ fontFamily: 'Inter-Bold', fontSize: 'bold', color: '#FD366E' }}>
+          Appwrite Functions
+        </h1>
       </div>
     ),
     {
       width: 1200,
       height: 630,
+      fonts: [
+        {
+          name: 'Inter',
+          data: await loadGoogleFont('Inter'),
+          style: 'normal',
+        },
+        {
+          name: 'Inter-Bold',
+          data: await loadGoogleFont('Inter'),
+          style: 'bold',
+        },
+      ],
     }
   );
 
@@ -37,4 +55,21 @@ export async function getGameOgImage({ req, res }) {
     response.status,
     Object.fromEntries(response.headers)
   );
+}
+
+async function loadGoogleFont(font) {
+  const url = `https://fonts.googleapis.com/css2?family=${font}`;
+  const css = await (await fetch(url)).text();
+  const resource = css.match(
+    /src: url\((.+)\) format\('(opentype|truetype)'\)/
+  );
+
+  if (resource) {
+    const response = await fetch(resource[1]);
+    if (response.status == 200) {
+      return await response.arrayBuffer();
+    }
+  }
+
+  throw new Error('failed to load font data');
 }
