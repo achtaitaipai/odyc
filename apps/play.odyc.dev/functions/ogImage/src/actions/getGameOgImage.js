c26c22697cf59c91c226f3a06da47cc3980b2d72
Matej Bačo <matejbaco2000@gmail.com>
2025-07-25 23:53:03 +0200

    Finish dynamic OG image

--- apps/play.odyc.dev/functions/ogImage/src/actions/getGameOgImage.js
+++ apps/play.odyc.dev/functions/ogImage/src/actions/getGameOgImage.js
@@ -1,41 +1,156 @@
 import { ImageResponse } from '@vercel/og';
 
-export async function getGameOgImage({ req, res }) {
+export async function getGameOgImage({ req, res, log }, { databases }) {
   const gameId = req.path.split('/')[4] ?? '';
 
   if (!gameId) {
     return res.send('URL param "gameId" is required.', 400);
   }
 
+  let game;
+  try {
+    game = await databases.getDocument('main', 'games', gameId);
+  } catch (error) {
+    log(error);
+    return res.send('Game not found.', 404);
+  }
+
+  const name = game.name ? game.name : 'Unnamed game';
+  const description = game.description
+    ? game.description
+    : 'Game without description.';
+  const thumbnailFileId = game.thumbnailFileId ? game.thumbnailFileId : null;
+  const ownerProfileId = game.ownerProfileId ? game.ownerProfileId : '';
+
+  const thumbnailUrl =
+    thumbnailFileId === null
+      ? 'https://odyc-play.appwrite.network/game-placeholder.png'
+      : thumbnailFileId.startsWith('/')
+        ? 'https://odyc-play.appwrite.network' + thumbnailFileId
+        : `https://fra.cloud.appwrite.io/v1/storage/buckets/screenshots/files/${thumbnailFileId}/view?project=odyc-js`;
+
+  const descriptionChunks = description.split('\n');
+
+  let profile;
+  try {
+    profile = await databases.getDocument('main', 'profiles', ownerProfileId);
+  } catch (error) {
+    log(error);
+    return res.send('Profile not found.', 404);
+  }
+
+  const { name: profileName } = profile;
+
   const response = new ImageResponse(
     (
       <div
         style={{
-          fontSize: 52,
-          color: '#EDEDF0',
-          background: '#19191D',
+          color: '#FFFFFF',
+          background: '#000000',
           width: '100%',
           height: '100%',
-          padding: '50px 200px',
-          textAlign: 'center',
-          fontFamily: 'Inter',
-          flexDirection: 'column',
-          gap: '-70px',
           display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          flexDirection: 'row',
+          gap: '0px',
+          margin: 0,
+          padding: 0,
         }}
       >
-        <h1 style={{ opacity: 0.6, fontSize: 40 }}>Dynamic OG images with</h1>
-        <h1
+        <div
           style={{
-            fontFamily: 'Inter-Bold',
-            fontSize: 'bold',
-            color: '#FD366E',
+            display: 'flex',
+            width: '570px',
+            height: '630px',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            margin: 0,
+            padding: '50px',
           }}
         >
-          Appwrite Functions
-        </h1>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '22px',
+              margin: 0,
+              padding: 0,
+            }}
+          >
+            <h1
+              style={{
+                fontFamily: 'Inter-Semibold',
+                fontWeight: '600',
+                fontSize: '56px',
+                margin: 0,
+                padding: 0,
+              }}
+            >
+              {name}
+            </h1>
+
+            <h3
+              style={{
+                fontFamily: 'Inter',
+                fontWeight: '400',
+                fontSize: '28px',
+                opacity: '0.5',
+                margin: 0,
+                padding: 0,
+              }}
+            >
+              Developed by {profileName}
+            </h3>
+
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                fontSize: '28px',
+                fontFamily: 'Inter',
+                fontWeight: '400',
+                gap: '0px',
+                margin: 0,
+                padding: 0,
+                marginTop: '16px',
+              }}
+            >
+              {descriptionChunks.map((chunk) =>
+                chunk ? (
+                  <p style={{ margin: 0, padding: 0 }}>{chunk}</p>
+                ) : (
+                  <p style={{ margin: 0, padding: 0, height: '30px' }}></p>
+                )
+              )}
+            </div>
+          </div>
+
+          <div style={{ display: 'flex', gap: '14px', margin: 0, padding: 0 }}>
+            <img
+              style={{
+                width: '35px',
+                height: '35px',
+                imageRendering: 'pixelated',
+              }}
+              src="https://odyc-play.appwrite.network/logo-70.png"
+            />
+            <span
+              style={{
+                fontFamily: 'Pixelify-Semibold',
+                fontSize: '32px',
+                fontWeight: '600',
+              }}
+            >
+              Odyc.js
+            </span>
+          </div>
+        </div>
+        <img
+          style={{
+            width: '630px',
+            height: '630px',
+          }}
+          src={thumbnailUrl}
+        />
       </div>
     ),
     {
@@ -45,12 +160,17 @@ export async function getGameOgImage({ req, res }) {
         {
           name: 'Inter',
           data: await loadGoogleFont('Inter'),
-          style: 'normal',
+          style: 'regular',
         },
         {
-          name: 'Inter-Bold',
+          name: 'Inter-Semibold',
           data: await loadGoogleFont('Inter'),
-          style: 'bold',
+          style: 'semibold',
+        },
+        {
+          name: 'Pixelify-Semibold',
+          data: await loadGoogleFont('Pixelify+Sans'),
+          style: 'semibold',
         },
       ],
     }
--- apps/play.odyc.dev/functions/ogImage/src/main.jsx
+++ apps/play.odyc.dev/functions/ogImage/src/main.jsx
@@ -1,8 +1,15 @@
 import { getGameOgImage } from './actions/getGameOgImage.js';
+import { Client, Databases } from 'node-appwrite';
 
 export default async (context) => {
+  const client = new Client()
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(req.headers['x-appwrite-key']);
+
+    const databases = new Databases(client);
+
   if (context.req.path.startsWith('/v1/og-images/games/')) {
-    return getGameOgImage(context);
+    return getGameOgImage(context, { client, databases });
   }
 
   return context.res.send('Not Found', 404);
