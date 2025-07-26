85cf70854368a2aeb345ae7bfc00efe76976e9d6
Matej Bačo <matejbaco2000@gmail.com>
2025-07-26 14:51:12 +0200

    Enable SSR

--- apps/play.odyc.dev/svelte.config.js
+++ apps/play.odyc.dev/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,18 +8,16 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+		// SSR
+		adapter: adapter()
+		// Static
+		// adapter: adapter({
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: 'index.html',
+		// 	precompress: false,
+		// 	strict: true
+		// })
 	}
 };
 
