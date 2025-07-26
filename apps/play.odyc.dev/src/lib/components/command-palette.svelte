23ac49932ea8d41d774c2b7cd086cb5a6dd1e01e
Matej Bačo <matejbaco2000@gmail.com>
2025-07-26 18:05:01 +0200

    fix command pallete game list of other users

--- apps/play.odyc.dev/src/lib/components/command-palette.svelte
+++ apps/play.odyc.dev/src/lib/components/command-palette.svelte
@@ -32,7 +32,13 @@
 	});
 
 	$effect(() => {
-		if (open) gamesPromise = Backend.listGames([Query.limit(50), Query.select(['$id', 'name'])]);
+		if (open) gamesPromise = Backend.listGames([
+			Query.limit(50),
+			Query.select(['$id', 'name']),
+			Query.equal('ownerProfileId', stores.profile?.$id ?? ''),
+			Query.orderDesc('$updatedAt'),
+			Query.orderDesc('$createdAt')
+		]);
 	});
 
 	afterNavigate(() => {
